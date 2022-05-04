c7f20a7726150a41a3a83b56fe14eccb5c5970b7
Ahmetcan Aksu <ahmetcanco@gmail.com>
2022-05-04 11:00:13 +0300

    Bump version

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(missing_docs)]
 #![deny(rustdoc::missing_doc_code_examples)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
-#![doc(html_root_url = "https://docs.rs/safe_en/1.6.6")]
+#![doc(html_root_url = "https://docs.rs/safe_en/1.6.7")]
 //!# SafeEn
 //!Local database solution with clean and strict data integrity.
 //!
